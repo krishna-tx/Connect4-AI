d12ab417b17f629cdc1b1a775a1d3dedbeb0f7be
krishna-tx <krishna.ramesh.tx@gmail.com>
2024-04-28 11:56:01 -0500

    added checking for end states

--- connect4.js
+++ connect4.js
@@ -1,5 +1,6 @@
-var currPlayer = "R";
-var freeRows = [5, 5, 5, 5, 5, 5, 5];
+var currPlayer = 'R';
+var availableRow = [5, 5, 5, 5, 5, 5, 5];
+var boardMatrix = [];
 
 window.onload = function() {
     setup();
@@ -19,30 +20,149 @@ function setup() {
     // cells
     let board = document.getElementById("board");
     for(let i = 0; i < 6; i++) {
+        let arr = []
         for(let j = 0; j < 7; j++) {
+            arr.push('');
             let cell = document.createElement("div");
             cell.id = i + "-" + j;
             cell.classList.add("cell");
             board.append(cell);
         }
+        boardMatrix.push(arr);
     }
 }
 
 function placeMove() {
     let row, col;
     col = parseInt(this.id);
-    row = freeRows[col];
+    row = availableRow[col];
     
     if(row < 0) { return; }
-    freeRows[col]--;
+    availableRow[col]--;
 
     let cell = document.getElementById(row + "-" + col);
-    if(currPlayer == "R") {
+    if(currPlayer == 'R') {
         cell.classList.add("red-piece");
-        currPlayer = "Y";
+        boardMatrix[row][col] = 'R';
+        currPlayer = 'Y';
     }
     else {
         cell.classList.add("yellow-piece");
-        currPlayer = "R";
+        boardMatrix[row][col] = 'Y';
+        currPlayer = 'R';
+    }
+    checkState();
+}
+
+function checkState() {
+    // check rows
+    for(let i = 0; i < 6; i++) {
+        for(let j = 0; j < 4; j++) {
+            let val = boardMatrix[i][j];
+            if(val != '' && val == boardMatrix[i][j+1] && val == boardMatrix[i][j+2] && val == boardMatrix[i][j+3]) {
+                let cell1 = document.getElementById(i + "-" + j);
+                let cell2 = document.getElementById(i + "-" + (j+1));
+                let cell3 = document.getElementById(i + "-" + (j+2));
+                let cell4 = document.getElementById(i + "-" + (j+3));
+
+                cell1.style.backgroundColor = "black";
+                cell2.style.backgroundColor = "black";
+                cell3.style.backgroundColor = "black";
+                cell4.style.backgroundColor = "black";
+
+                if(val == 'R') { console.log("Red won!"); }
+                else { console.log("Yellow won!"); }
+                endGame();
+                return;
+            }
+        }
+    }
+
+    // check columns
+    for(let j = 0; j < 7; j++) {
+        for(let i = 0; i < 3; i++) {
+            let val = boardMatrix[i][j];
+            if(val != '' && val == boardMatrix[i+1][j] && val == boardMatrix[i+2][j] && val == boardMatrix[i+3][j]) {
+                let cell1 = document.getElementById(i + "-" + j);
+                let cell2 = document.getElementById((i+1) + "-" + j);
+                let cell3 = document.getElementById((i+2) + "-" + j);
+                let cell4 = document.getElementById((i+3) + "-" + j);
+
+                cell1.style.backgroundColor = "black";
+                cell2.style.backgroundColor = "black";
+                cell3.style.backgroundColor = "black";
+                cell4.style.backgroundColor = "black";
+
+                if(val == 'R') { console.log("Red won!"); }
+                else { console.log("Yellow won!"); }
+                endGame();
+                return;
+            }
+        }
+    }
+
+    // check \ diagonal
+    for(let i = 0; i < 3; i++) {
+        for(let j = 0; j < 4; j++) {
+            let val = boardMatrix[i][j];
+            if(val != '' && val == boardMatrix[i+1][j+1] && val == boardMatrix[i+2][j+2] && val == boardMatrix[i+3][j+3]) {
+                let cell1 = document.getElementById(i + "-" + j);
+                let cell2 = document.getElementById((i+1) + "-" + (j+1));
+                let cell3 = document.getElementById((i+2) + "-" + (j+2));
+                let cell4 = document.getElementById((i+3) + "-" + (j+3));
+
+                cell1.style.backgroundColor = "black";
+                cell2.style.backgroundColor = "black";
+                cell3.style.backgroundColor = "black";
+                cell4.style.backgroundColor = "black";
+
+                if(val == 'R') { console.log("Red won!"); }
+                else { console.log("Yellow won!"); }
+                endGame();
+                return;
+            }
+        }
+    }
+
+    // check / diagonal
+    for(let i = 0; i < 3; i++) {
+        for(let j = 3; j < 7; j++) {
+            let val = boardMatrix[i][j];
+            if(val != '' && val == boardMatrix[i+1][j-1] && val == boardMatrix[i+2][j-2] && val == boardMatrix[i+3][j-3]) {
+                let cell1 = document.getElementById(i + "-" + j);
+                let cell2 = document.getElementById((i+1) + "-" + (j-1));
+                let cell3 = document.getElementById((i+2) + "-" + (j-2));
+                let cell4 = document.getElementById((i+3) + "-" + (j-3));
+
+                cell1.style.backgroundColor = "black";
+                cell2.style.backgroundColor = "black";
+                cell3.style.backgroundColor = "black";
+                cell4.style.backgroundColor = "black";
+
+                if(val == 'R') { console.log("Red won!"); }
+                else { console.log("Yellow won!"); }
+                endGame();
+                return;
+            }
+        }
+    }
+
+    // check for draw
+    for(let i = 0; i < 6; i++) {
+        for(let j = 0; j < 7; j++) {
+            if(boardMatrix[i][j] == '') { return; }
+        }
+    }
+
+    // all cells filled => game is draw
+    console.log("Draw!");
+    endGame();
+
+}
+
+function endGame() {
+    selectors = document.getElementsByClassName("selector");
+    for(let i = 0; i < selectors.length; i++) {
+        selectors[i].removeEventListener("click", placeMove); // don't listen for clicks
     }
 }
